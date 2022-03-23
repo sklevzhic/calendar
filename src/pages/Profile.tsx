f07045062e6ba0347395ba7cb69a045d6b383e9e
Alexander <sklevzhic@gmail.com>
2022-03-23 22:41:28 +0300

    fix

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,6 +1,8 @@
-import { Empty } from 'antd';
-
-import React from 'react'
+import {Button, Checkbox, Col, List, Row} from 'antd';
+import TextArea from 'antd/lib/input/TextArea';
+import Title from 'antd/lib/typography/Title';
+import axios from "axios"
+import React, {useEffect, useState} from 'react'
 
 
 interface ProfileProps {
@@ -8,8 +10,84 @@ interface ProfileProps {
 }
 
 export const Profile: React.FC<ProfileProps> = () => {
+    let [specialities, setSpecialities] = useState([])
+    useEffect(() => {
+        axios.get("https://printersipkip.herokuapp.com/dates").then(res => setSpecialities(res.data.map((el: any, i: any) => ({
+        // axios.get("http://localhost:1337/dates").then(res => setSpecialities(res.data.map((el: any, i: any) => ({
+            ...el,
+            checked: (i <= 2) ? true : false
+        }))))
+    }, [])
 
-    return <div  style={{padding: '0 50px'}}>
-        <Empty description={"Profile"}/>
+    let code = specialities.filter((el: any) => el.checked === true).map((el1: any) => {
+
+        return ` <div class="modal-body item">
+        <div class="dimming_effect">
+            <figure class="picture_caption">
+            <a class="modal_link" href=${el1.link}>
+            <img alt="1" class="modal_picture" src=${el1.image}>
+            </a>
+                <figcaption class="start_date">
+                <a class="modal_link modal_span" href=${el1.link}>
+                начало обучения:<br/>
+                    ${el1.date.dateText}</a>
+                    </figcaption>
+            </figure>
+
+
+            <div class="flx_cont">
+                <p class="speciality_name"><a class="modal_link"
+                                                  href=${el1.link}>${el1.speciality}</a></p>
+            </div>
+        </div>
+    </div>`
+    }).join(" ")
+
+    let changeSpeciality = (id: string, value: boolean) => {
+        let arr: any = specialities.map((el: any) => {
+            if (el.speciality === id) {
+                return {...el, checked: value}
+            } else {
+                return el
+            }
+        })
+        setSpecialities(arr)
+
+    };
+
+    return <div style={{padding: '0 50px'}}>
+        {/*<Empty description={"Profile"}/>*/}
+        <Row gutter={12}>
+            <Col span={16}>
+                <Title level={4}>Специальности</Title>
+                <List
+                    size="small"
+                    bordered
+                    dataSource={specialities}
+                    renderItem={(item: any) => <Speciality item={item} changeSpeciality={(val: boolean) => changeSpeciality(item.speciality,val)}/>}
+                />
+            </Col>
+            <Col span={8}>
+                <Title level={4}>Code</Title>
+                <TextArea value={code as any} autoSize={{minRows: 15, maxRows: 6}}/>
+                <Button>
+                    Copy
+                </Button>
+            </Col>
+        </Row>
     </div>
 };
+
+
+const Speciality: React.FC<{ item: any, changeSpeciality: any }> = ({item, changeSpeciality}: any) => {
+
+    return <List.Item>
+        <Checkbox checked={item.checked}
+                  onChange={(e: any) => changeSpeciality(e.target.checked)}
+        >
+            {item.date.dateText}
+        </Checkbox>
+        {item.speciality}
+    </List.Item>
+};
+
